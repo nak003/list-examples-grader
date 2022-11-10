03a1a1443a39225881a1be1ba7cb09c35c059706
nak003 <nak003@ucsd.edu>
2022-11-10 13:45:07 -0800

    add

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,36 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.*; 
 
 public class TestListExamples {
-  // Write your grading tests here!
+  @Test
+    public void testMerge(){
+        List<String> list1 = new ArrayList<String>();
+        list1.add("x");
+        list1.add("y");
+        List<String> list2 = new ArrayList<String>();
+        list2.add("a");
+        list2.add("z"); 
+        List<String> expected = new ArrayList<String>();
+        expected.add("a");
+        expected.add("x");
+        expected.add("y");
+        expected.add("z");
+        assertEquals(expected, ListExamples.merge(list1, list2));
+  }
+
+  @Test
+    public void testFilter(){
+        List<String> list1 = new ArrayList<String>();
+        list1.add("apple"); 
+        list1.add("none");
+        list1.add("pineapple");
+        list1.add("hi");
+        list1.add("app");
+        List<String> expected = new ArrayList<String>();
+        expected.add("apple");
+        expected.add("pineapple");
+        expected.add("app");
+        assertEquals(expected, ListExamples.filter(list1, sc));
+  }
 }
--- grade.sh
+++ grade.sh
@@ -3,5 +3,22 @@
 set -e
 
 rm -rf student-submission
+
 git clone $1 student-submission
 
+cd student-submission
+
+if [[ -f ListExamples.java ]]
+then 
+    echo "ListExamples.java file submitted"
+else 
+    echo "ListExamples.java file not submitted"
+    exit 1
+fi
+
+
+cp ListExamples.java ../
+cd ../
+
+javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java 
+java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
