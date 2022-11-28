64b52df3cf53beb3e4bfce6e393d549e87db1f49
nak003 <nak003@ucsd.edu>
2022-11-27 22:55:08 -0800

    lab

--- grade.sh
+++ grade.sh
@@ -1,24 +1,35 @@
 # Create your grading script here
-
-set -e
-
 rm -rf student-submission
-
+mkdir student-submission
 git clone $1 student-submission
-
+cp TestListExamples.java student-submission
 cd student-submission
 
-if [[ -f ListExamples.java ]]
+if [[ -e ListExamples.java ]]
 then 
-    echo "ListExamples.java file submitted"
+    echo "ListExamples.java file submitted! +1 point"
 else 
-    echo "ListExamples.java file not submitted"
+    echo "ListExamples.java file is missing"
     exit 1
 fi
+CPATH=".:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar"
+javac -cp $CPATH *.java
+if [[ $? -eq 0 ]]
+then
+    echo "Files are successfully complied! +1 point"
 
+else 
+    echo "Complie Error"
+    exit 1
+fi 
 
-cp ListExamples.java ../
-cd ../
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
+if [[ $? -eq 0 ]]
+then
+    echo "All test passed! Total: 3/3 point"
+    exit 0
 
-javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java 
-java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples
+else 
+    echo "Test Error"
+    exit 1
+fi 
