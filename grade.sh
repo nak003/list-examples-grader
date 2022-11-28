e4e0ca2faed39409d8055d85eac71be61eea6737
nak003 <nak003@ucsd.edu>
2022-11-27 23:18:27 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -4,12 +4,14 @@ mkdir student-submission
 git clone $1 student-submission
 cp TestListExamples.java student-submission
 cd student-submission
+GRADE=0 
 
 if [[ -e ListExamples.java ]]
 then 
     echo "ListExamples.java file submitted! +1 point"
+    ((GRADE++))
 else 
-    echo "ListExamples.java file is missing"
+    echo "ListExamples.java file is missing -1 point Total:" $GRADE"/3 point"
     exit 1
 fi
 CPATH=".:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar"
@@ -17,21 +19,21 @@ javac -cp $CPATH *.java
 if [[ $? -eq 0 ]]
 then
     echo "Files are successfully complied! +1 point"
-
+    ((GRADE++))
 else 
-    echo "Complie Error"
+    echo "Complie Error -1 point Total:" $GRADE"/3 point"
     exit 1
 fi 
 
 java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > test.txt
 if [[ $? -eq 0 ]]
 then
-    echo "All test passed! Total: 3/3 point"
-    cat test.txt
+    ((GRADE++)) 
+    echo "All test passed! Total:" $GRADE"/3 point"
     exit 0
 
 else 
-    echo "Test Error"
-    cat test.txt
+    echo "Test Error -1 point"
+    echo "More work needed! Total:" $GRADE"/3 point"
     exit 1
 fi 
